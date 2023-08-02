604a5a68897bf89c81784aec176858fc40fd26d4
JD <jonathondavis1101@gmail.com>
2023-08-02 14:18:45 -0700

    Added a lot more practice coding

--- main.py
+++ main.py
@@ -133,3 +133,101 @@ print(health <= 0 or lives <= 0)
 
 # collections practice (lists, tuples, ranges, dictionaries)
 
+# lists ["each item will be an 'element'", "The first element will have the index position of 0", "items in a list are mutable"]
+
+inventory = ['Iron Sword', 'Iron Helm', 'Iron Shield']
+print(inventory) 
+inventory[1] = 'Apples'
+print(inventory)
+inventory[2] = 'Potion'
+print(inventory)
+print(len(inventory))
+print(max(inventory))
+# will return the element with the highest alphatbetical value (a=0 , b=1, etc)
+print(min(inventory))
+# will return element with the lowest alphabetic value
+
+# Append will add an element to the end of the list
+inventory.append('Leather')
+print(inventory)
+
+# Insert will allow you to place an element at a specific index point (first parameter is the index point , second is the desired element)
+inventory.insert(2, 'Hide')
+print(inventory)
+
+# Pop removes the last element on the list
+inventory.pop()
+print(inventory)
+
+# Remove will specify a specific str to remove from the list
+inventory.remove('Hide')
+print(inventory)
+
+# Clear will delete all items in the list, (example = [] also works)
+inventory.clear()
+print(inventory)
+
+# Multi-dimensional lists (lists within lists) , they do not have to be the same length
+inventory = [[1, 2, 3], 
+             [1, 2, 3, 4],
+             [1, 2],
+             [1, 2, 3],
+             [1, 2, 3]]
+ruby_pouch = inventory[4][1]
+print(ruby_pouch)
+leather_bag = inventory[1][3]
+print(leather_bag)
+
+# append will also worth with multidimensional lists
+inventory.append([1, 2, 3, 4, 5])
+print(inventory)
+
+# Each list will also have an index position as a whole
+inventory[0].pop()
+print(inventory)
+inventory[2].pop()
+print(inventory)
+
+# Using the Slicing method we can add a list into a specific index point in our multidimensional lists
+new_inventory = [1, 2, 3, 4, 5, 6]
+index_to_insert = 3
+inventory[index_to_insert:index_to_insert] = [new_inventory]
+print(inventory)
+
+# Tuples are immutable and only used to store data that won't change, format with key and value pairs ex. = ("example", 3)
+
+item = ("Potions", 5)
+name = item[0]
+amount = item[1]
+print(name)
+print(amount)
+print(item.count("Potions"))
+print(item.index(5))
+
+# Dictionaries use {} with key value pairs , searching requires key position on the list, not the index position
+
+inventory = {'Sword': 1, 'Shield': 1, 'Healthkit': 10, 'Berries': 5}
+print(inventory['Berries'])
+print(inventory['Healthkit'])
+
+inventory['gold'] = 100
+print(inventory)
+
+# Dictionaries can add more elements to their lists by giving a new element a value
+
+inventory['Rations'] = 5
+print(inventory)
+
+# Modification of Dictionaries also requires the use of the key, and not the index position
+
+inventory['Sword'] = 2
+print(inventory)
+
+inventory['gold'] += 1
+print(inventory)
+
+inventory['Rats'] = 32
+print(inventory)
+inventory['Rats'] -= 14
+print(inventory)
+
